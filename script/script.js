7fe701123776c3d0c5d920ddef7911005a62ecd8
PavelEremeev <pe19950520@yandex.ru>
2022-02-07 23:40:09 +0300

    feat: Добавил сохранение данных в localStorage

--- script/script.js
+++ script/script.js
@@ -1,30 +1,45 @@
 const addTaskInput = document.querySelector('.todomato-list_task');
 const addTaskButton = document.querySelector('.todomato-list_button')
-const todomatoTask = document.querySelector('.todomato-task')
-const todomatoList = []
+const todomatoContainer = document.querySelector('.todomato-task')
 
-addTaskButton.addEventListener('click', () => {
+// Создаем массив через let чтобы можно было менять его содержимое.
+let todomatoList = []
 
-    const newTodomatoList = {
-        todomato: addTaskInput.value
-    }
-    todomatoList.push(newTodomatoList)
-    displayTask()
-    console.log(todomatoList)
+// Проверяем есть ли локальные таски в сторейдже.
+function getLocalTasks() {
+	if (localStorage.getItem('todomato')) {
+		todomatoList = JSON.parse(localStorage.getItem('todomato'))
+		displayTask()
+	}
+}
 
-})
+getLocalTasks()
 
+// Функция добавляющая таску в массив.
 function displayTask() {
-    let displayTask = "";
-    todomatoList.forEach(function (item, i) {
-        console.log(item)
+	let displayTask = "";
+	todomatoList.forEach(function (item, i) {
+		console.log(item)
 
-        displayTask += `
+		displayTask += `
         <li>
             <button id="item_${i}">Удалить</button>
             <label for="item_${i}">${item.todomato}></label>
         </li>
         `;
-        todomatoTask.innerHTML = displayTask
-    })
+		todomatoContainer.innerHTML = displayTask
+		localStorage.setItem('todomato', JSON.stringify(todomatoList))
+	})
 }
+
+
+addTaskButton.addEventListener('click', () => {
+
+	const newTodomatoList = {
+		todomato: addTaskInput.value
+	}
+	todomatoList.push(newTodomatoList)
+	displayTask()
+	console.log(todomatoList)
+
+})
