b1135ce8f4970eea4bfe94297f80dacc00ab3f3a
PavelEremeev <pe20051995@yandex.ru>
2022-06-20 23:19:58 +0300

    Dnd disabled. Doing classic toDo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,49 +1,37 @@
 import React from 'react';
-import Draggable from 'react-draggable';
 import './TodoList.css';
 
 
-function TodoList({ todo, setTodo, cleanUp }) {
+function TodoList({ todo, setTodo }) {
+
 
 	const handleDeleteTask = (id) => {
 		console.log(id)
 		setTodo(todo.filter((task) => task.id !== id))
 	}
 
-
-	const handleUpdatePosition = (data, i) => {
-		let todoArr = [...todo]
-		todoArr[i].defPos = { x: data.x, y: data.y }
-		setTodo(todoArr)
-	}
-
 	return (
-		<section className="todo-list">
+		<section className='todo-list'>
 			<div className='todo-list__wrapper'>
 				{todo.map((task, i) =>
-					<Draggable key={task.id}
-						defaultPosition={task.defPos}
-						onStop={(_, data) => {
-							handleUpdatePosition(data, i);
-							return false
-						}}
-					>
-						<div className='todo-list__task-wrapper'
-							style={{ backgroundColor: task.color }}>
-							<div className='todo-list__task-number'>
-								{i + 1 + '. '}
-							</div>
-							<div className='todo-list__task'>
-								{task.taskDescription}
-							</div>
-							<button className='todo-list__delete-button' onClick={() => handleDeleteTask(task.id)}>
-								X
-							</button>
+					<div id={task.id} key={task.id} className='todo-list__task-wrapper'
+						style={{ backgroundColor: task.color }}>
+						<div className='todo-list__task-number'>
+							{i + 1 + '. '}
 						</div>
-					</Draggable>
+						<div className='todo-list__task'>
+							{task.taskDescription}
+						</div>
+						<button className='todo-list__delete-button' onClick={() => {
+
+							handleDeleteTask(task.id)
+						}}>
+							X
+						</button>
+					</div>
 				)}
 			</div>
-		</section>
+		</section >
 	);
 }
 
