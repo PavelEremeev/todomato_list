4fa9644dbf8f75981fcfa586bd2add467ceee232
PavelEremeev <pe20051995@yandex.ru>
2022-06-27 21:35:21 +0300

    Изменена стилизация. Убраны варнинги.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import footerLogo from '../images/telegram_logo.png'
 
 
 import './Footer.css';
 
 
-function Footer() {  
+function Footer() {
 
 	const date = new Date();
-	
-	
+
+
 	return (
-    <footer className='footer'>
+		<footer className='footer'>
 			<div className='footer__container'>
 				<p className='footer__date'>{date.getFullYear() + ' '}&copy;</p>
 				<a href='https://t.me/kermit_kvebit'
-				className='footer__link'
-				target='_blank'>
-					<img src={footerLogo}  className='footer__image'/>
+					className='footer__link'
+					rel="noreferrer"
+					target='_blank'>
+					<img src={footerLogo} className='footer__image' alt='footerimage' />
 				</a>
 			</div>
-			
-    </footer>
-  );
+
+		</footer>
+	);
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './Header.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logo from '../images/todomato_logo.png'
 
 function Header() {
-  return (
-    <header className='header'>
-		<Link to='/' className='header__logo'>
-			<h1 className='header__title'>TOD</h1>
-			<img className='header__image' src={logo}/>
-			<h1 className='header__title'>MAT</h1>
-			<img className='header__image' src={logo}/>
-		</Link>
-		<div className='header__container'>
-			<Link to='/timer' className='header__text'>Попробуйте TODOMATO-timer</Link>
-		</div>
-    </header>
-  );
+	return (
+		<header className='header'>
+			<Link to='/' className='header__logo'>
+				<h1 className='header__title'>TOD</h1>
+				<img className='header__image' src={logo} alt='tomatopicture' />
+				<h1 className='header__title'>MAT</h1>
+				<img className='header__image' src={logo} alt='tomatopicture' />
+			</Link>
+			<div className='header__container'>
+				<Link to='/timer' className='header__text'>Попробуйте TODOMATO-timer</Link>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
