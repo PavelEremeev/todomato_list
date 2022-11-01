3e0fc5619cbddf57f5c921023b6470a9f0d96046
PavelEremeev <pe19950520@yandex.ru>
2022-11-01 23:15:33 +0300

    Исправлены стили кнопок. Добавлен выбор и состояние

--- src/components/Timer/Presets.js
+++ src/components/Timer/Presets.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Presets.css'
 
 export default function Presets({ presetsName }) {
+    const [active, setActive] = useState(0)
+
+    const handleClick = (index) => {
+        setActive(index)
+        console.log(active)
+    }
+
     return (
         <section className='presets'>
             <div className='presets__wrapper'>
                 {presetsName.map((tag, index) =>
-                    <button className='preset__name' key={index}>{tag}</button>)}
+                    <button className={active === index ? 'preset__name_active' : 'preset__name'}
+                        onClick={() => handleClick(index)}
+                        active={active ? index : undefined}
+                        key={index}>{tag}</button>)}
             </div>
         </section>
     )
