a69973714b01fa4113ce07113a032983245b3672
PavelEremeev <pe19950520@yandex.ru>
2022-11-06 17:47:11 +0300

    Добавлен функционал таймера в компонент Circle

--- src/components/Timer/Circle.js
+++ src/components/Timer/Circle.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './Circle.css'
 
 export default function Circle() {
 
     const [progress, setProgress] = useState(55)
+    const [timer, setTimer] = useState(100);
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        if (active && timer > 0) {
+            const interval = setInterval(() => {
+                setTimer((timer) => timer - 1)
+            }, 1000);
+
+
+            return () => {
+                clearInterval(interval)
+            }
+        }
+    }, [timer, active])
+
+    const toggleTimer = () => {
+        setActive(!active)
+    }
+
+    const getTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+
+        return `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+    }
+
 
     return (
         <section className='circle'>
@@ -16,9 +43,9 @@ export default function Circle() {
                     <div className='circle__inner'>
                         <div className='circle__clock-container'>
                             <h2 className='circle__clock'>
-                                5:00
+                                {getTime(timer)}
                             </h2>
-                            <button className='circle__button'>ПАУЗА</button>
+                            <button onClick={toggleTimer} className='circle__button'>{active ? 'ПАУЗА' : 'СТАРТ'}</button>
                         </div>
                     </div>
                 </div>
