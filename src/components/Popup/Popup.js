6ab270c58d273d91347c8d387c362181f32a4bcf
PavelEremeev <pe19950520@yandex.ru>
2022-11-07 20:40:11 +0300

    Добавлены стили и базовая функциональность модального окна

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -5,17 +5,16 @@ import './Popup.css'
 export default function Popup({ isOpen, popupToggleHandler }) {
 
 
-
     return (
         <>
-            {isOpen ? <section className='popup'>
-                <div className='popup__wrapper'>
-                    <h3 className='popup__title'> title</h3>
-                    <button onClick={popupToggleHandler} className='popup__close-button'>x</button>
-                    <input className='popup__input'></input>
-                    <input className='popup__input'></input>
-                    <input className='popup__input'></input>
-                    <button className='popup__apply-button'>применить</button>
+            {isOpen ? <section className='popup' onClick={popupToggleHandler}>
+                <div className='popup__wrapper' onClick={evt => evt.stopPropagation()}>
+                    <h3 className='popup__title'>НАСТРОЙКИ ТАЙМЕРА</h3>
+                    <button onClick={popupToggleHandler} className='popup__close-button' />
+                    <input className='popup__input' placeholder='' />
+                    <input className='popup__input' />
+                    <input className='popup__input' />
+                    <button className='popup__apply-button'>Применить</button>
                 </div>
             </section> : ''}
         </>
