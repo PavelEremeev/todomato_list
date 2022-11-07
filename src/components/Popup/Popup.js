8ff76d6adea267f676cad8f02020a2a15ab06c26
PavelEremeev <pe19950520@yandex.ru>
2022-11-07 21:17:31 +0300

    Добавлены функции редактирования. Исправлены стили

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import { hasFormSubmit } from '@testing-library/user-event/dist/utils'
+import React, { useContext } from 'react'
+import { StateContext } from '../StateProvider/StateProvider'
 
 import './Popup.css'
 
 export default function Popup({ isOpen, popupToggleHandler }) {
+
+    const { shortBreakTime,
+        setShortBreakTime,
+        longBreakTime,
+        setLongBreakTime,
+        workTime,
+        setWorkTime } = useContext(StateContext)
+
+    const handleWorkTimerChange = (evt) => {
+        setWorkTime(evt.target.value * 60)
+    }
+
+    const handleShortBreakTimerChange = (evt) => {
+
+        setShortBreakTime(evt.target.value * 60)
+
+    }
+
+    const handleLongBreakTimerChange = (evt) => {
+        setLongBreakTime(evt.target.value * 60)
+    }
+
 
 
     return (
@@ -11,10 +35,10 @@ export default function Popup({ isOpen, popupToggleHandler }) {
                 <div className='popup__wrapper' onClick={evt => evt.stopPropagation()}>
                     <h3 className='popup__title'>НАСТРОЙКИ ТАЙМЕРА</h3>
                     <button onClick={popupToggleHandler} className='popup__close-button' />
-                    <input className='popup__input' placeholder='' />
-                    <input className='popup__input' />
-                    <input className='popup__input' />
-                    <button className='popup__apply-button'>Применить</button>
+                    <input className='popup__input' maxLength={2} placeholder={`Работа: ${workTime / 60} мин.`} onChange={(evt) => handleWorkTimerChange(evt)}></input>
+                    <input className='popup__input' maxLength={2} placeholder={`Короткий перерыв: ${shortBreakTime / 60} мин.`} onChange={(evt) => handleShortBreakTimerChange(evt)}></input>
+                    <input className='popup__input' maxLength={2} placeholder={`Длинный перерыв: ${longBreakTime / 60} мин.`} onChange={(evt) => handleLongBreakTimerChange(evt)}></input>
+                    {/* <button className='popup__apply-button' >Применить</button> */}
                 </div>
             </section> : ''}
         </>
